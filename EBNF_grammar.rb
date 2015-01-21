4fbb90348aff6e3a9eb3d8a8b69005880944fe50
letsleaveitempty <anita.jaszewska@gmail.com>
2015-01-21 10:56:07 +0000

    left and right sided recursion EBNF

--- EBNF_grammar.rb
+++ EBNF_grammar.rb
@@ -40,20 +40,20 @@ node ::= expression | number;
 
 ###
 ### left side recursion
-digit  ::= '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9';
-number ::= digit | number, digit;
-
-bracket ::= opening_bracket | closing_bracket;
-opening_bracket ::= '(';
-closing_bracket ::= ')';
-
-expression ::=   expression + primary_expression
-               | expression - primary_expression
-               | primary_expression;
-
-primary_expression ::=    primary_expression * expression
-                        | primary_expression / expression
-                        | opening_bracket, expression, closing_bracket
-                        | number;
-
-node ::= expression | number;
+# digit  ::= '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9';
+# number ::= digit | number, digit;
+#
+# bracket ::= opening_bracket | closing_bracket;
+# opening_bracket ::= '(';
+# closing_bracket ::= ')';
+#
+# expression ::=   expression + primary_expression
+#                | expression - primary_expression
+#                | primary_expression;
+#
+# primary_expression ::=    primary_expression * expression
+#                         | primary_expression / expression
+#                         | opening_bracket, expression, closing_bracket
+#                         | number;
+#
+# node ::= expression | number;
